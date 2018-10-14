a23fa902ddf5649c9f32d648f311564ab8a605db
Ernest Landrito <landrito@google.com>
2018-10-14 09:10:06 -0700

    Refactor Wait to be a LRO and add Operations Service

---
+++ server/operations_service.go
@@ -0,0 +1,69 @@
+// Copyright 2018 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package server
+
+import (
+	"context"
+	"encoding/base64"
+	"strings"
+
+	"github.com/golang/protobuf/proto"
+	"github.com/golang/protobuf/ptypes/empty"
+	pb "github.com/googleapis/gapic-showcase/server/genproto"
+	lropb "google.golang.org/genproto/googleapis/longrunning"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+// NewEchoServer returns a new EchoServer for the Showcase API.
+func NewOperationsServer() lropb.OperationsServer {
+	return &operationsServerImpl{waiter: GetWaiterSingleton()}
+}
+
+type operationsServerImpl struct {
+	waiter Waiter
+}
+
+func (s *operationsServerImpl) GetOperation(ctx context.Context, in *lropb.GetOperationRequest) (*lropb.Operation, error) {
+	if strings.HasPrefix(in.Name, "operations/google.showcase.v1alpha2.Echo/Wait/") {
+		return s.handleWait(in)
+	}
+	return nil, status.Errorf(codes.NotFound, "Operation %q not found.", in.Name)
+}
+
+func (s *operationsServerImpl) handleWait(in *lropb.GetOperationRequest) (*lropb.Operation, error) {
+	waitReq := &pb.WaitRequest{}
+	encodedBytes := strings.TrimPrefix(in.Name, "operations/google.showcase.v1alpha2.Echo/Wait/")
+	waitReqBytes, _ := base64.StdEncoding.DecodeString(encodedBytes)
+
+	err := proto.Unmarshal(waitReqBytes, waitReq)
+	if err != nil {
+		return nil, status.Errorf(codes.NotFound, "Operation %q not found.", in.Name)
+	}
+
+	return s.waiter.Wait(waitReq), nil
+}
+
+func (s operationsServerImpl) CancelOperation(ctx context.Context, in *lropb.CancelOperationRequest) (*empty.Empty, error) {
+	return nil, status.Error(codes.Unimplemented, "google.longrunning.CancelOperation is unimplemented.")
+}
+
+func (s operationsServerImpl) ListOperations(ctx context.Context, in *lropb.ListOperationsRequest) (*lropb.ListOperationsResponse, error) {
+	return nil, status.Error(codes.Unimplemented, "google.longrunning.ListOperations is unimplemented.")
+}
+
+func (s operationsServerImpl) DeleteOperation(ctx context.Context, in *lropb.DeleteOperationRequest) (*empty.Empty, error) {
+	return nil, status.Error(codes.Unimplemented, "google.longrunning.DeleteOperation is unimplemented.")
+}
---
+++ server/operations_service_test.go
@@ -0,0 +1,97 @@
+// Copyright 2018 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package server
+
+import (
+	"context"
+	"encoding/base64"
+	"fmt"
+	"testing"
+
+	"github.com/golang/protobuf/proto"
+	"github.com/golang/protobuf/ptypes/timestamp"
+	pb "github.com/googleapis/gapic-showcase/server/genproto"
+	lropb "google.golang.org/genproto/googleapis/longrunning"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func TestGetOperation(t *testing.T) {
+	nameBytes, _ := proto.Marshal(
+		&pb.WaitRequest{EndTime: &timestamp.Timestamp{Seconds: 1}})
+	req := &lropb.GetOperationRequest{
+		Name: fmt.Sprintf(
+			"operations/google.showcase.v1alpha2.Echo/Wait/%s",
+			base64.StdEncoding.EncodeToString(nameBytes)),
+	}
+
+	waiter := &mockWaiter{called: false}
+	server := &operationsServerImpl{waiter: waiter}
+	server.GetOperation(context.Background(), req)
+	if !waiter.called {
+		t.Error("Expected echo.Wait to defer to waiter.")
+	}
+}
+
+func TestGetOperation_notFoundOperation(t *testing.T) {
+	req := &lropb.GetOperationRequest{
+		Name: "BOGUS",
+	}
+	server := NewOperationsServer()
+	_, err := server.GetOperation(context.Background(), req)
+	s, _ := status.FromError(err)
+	if codes.NotFound != s.Code() {
+		t.Errorf("GetOperation with invalid name expected code=%d, got %d", codes.NotFound, s.Code())
+	}
+}
+
+func TestGetOperation_invalidEncodedName(t *testing.T) {
+	req := &lropb.GetOperationRequest{
+		Name: "operations/google.showcase.v1alpha2.Echo/Wait/BOGUS",
+	}
+	server := NewOperationsServer()
+	_, err := server.GetOperation(context.Background(), req)
+	s, _ := status.FromError(err)
+	if codes.NotFound != s.Code() {
+		t.Errorf("GetOperation with invalid name expected code=%d, got %d", codes.NotFound, s.Code())
+	}
+}
+
+func TestCancelOperation(t *testing.T) {
+	server := NewOperationsServer()
+	_, err := server.CancelOperation(context.Background(), nil)
+	s, _ := status.FromError(err)
+	if codes.Unimplemented != s.Code() {
+		t.Errorf("CancelOperation expected code=%d, got %d", codes.Unimplemented, s.Code())
+	}
+}
+
+func TestServerListOperation(t *testing.T) {
+	server := NewOperationsServer()
+	_, err := server.ListOperations(context.Background(), nil)
+	s, _ := status.FromError(err)
+	if codes.Unimplemented != s.Code() {
+		t.Errorf("ListOperations expected code=%d, got %d", codes.Unimplemented, s.Code())
+	}
+}
+
+func TestServerDeleteOperation(t *testing.T) {
+	server := NewOperationsServer()
+	_, err := server.DeleteOperation(context.Background(), nil)
+	s, _ := status.FromError(err)
+	if codes.Unimplemented != s.Code() {
+		t.Errorf("DeleteOperations expected code=%d, got %d", codes.Unimplemented, s.Code())
+	}
+}
