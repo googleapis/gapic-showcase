f0d03b932ac7d68f17ec05ff6855327c2915810e
Noah Dietz <noahdietz@users.noreply.github.com>
2019-07-03 11:36:02 -0700

    release v0.2.1 (#146)

--- cmd/gapic-showcase/version.go
+++ cmd/gapic-showcase/version.go
@@ -18,7 +18,7 @@ func init() {
 	// Make roots version option only emit the version. This is used in circleci.
 	// The template looks weird on purpose. Leaving as a single line causes the
 	// output to append an extra character.
-	rootCmd.Version = "0.2.0"
+	rootCmd.Version = "0.2.1"
 	rootCmd.SetVersionTemplate(
 		`{{printf "%s" .Version}}`)
 }
