a5241a4c8258685885e19f2f7d97fde59bbe8b03
Google APIs <googleapis-publisher@google.com>
2020-01-29 14:50:43 -0800

    Regenerate cmd, client, and server directories. (#277)

--- server/genproto/testing.pb.go
+++ server/genproto/testing.pb.go
@@ -1261,11 +1261,11 @@ var fileDescriptor_ec10fb45fe35d712 = []byte{
 
 // Reference imports to suppress errors if they are not otherwise used.
 var _ context.Context
-var _ grpc.ClientConn
+var _ grpc.ClientConnInterface
 
 // This is a compile-time assertion to ensure that this generated file
 // is compatible with the grpc package it is being compiled against.
-const _ = grpc.SupportPackageIsVersion4
+const _ = grpc.SupportPackageIsVersion6
 
 // TestingClient is the client API for Testing service.
 //
@@ -1300,10 +1300,10 @@ type TestingClient interface {
 }
 
 type testingClient struct {
-	cc *grpc.ClientConn
+	cc grpc.ClientConnInterface
 }
 
-func NewTestingClient(cc *grpc.ClientConn) TestingClient {
+func NewTestingClient(cc grpc.ClientConnInterface) TestingClient {
 	return &testingClient{cc}
 }
 
