00e8372e94a16ae90e310c118cb24a6f039c30a7
xiaozhenliugg <xiaozhenliu@google.com>
2020-05-12 11:33:58 -0700

    change resource name for createBlurb with non-slash pattern

--- server/services/messaging_service.go
+++ server/services/messaging_service.go
@@ -303,7 +303,14 @@ func (s *messagingServerImpl) CreateBlurb(ctx context.Context, in *pb.CreateBlur
 	id := puid.Next()
 	name := fmt.Sprintf("%s/blurbs/%d", parent, id)
 	now := ptypes.TimestampNow()
-
+	if b.LegacyRoomId {
+		roomId := b.LegacyRoomId
+		name := fmt.Sprintf("%s/blurbs/%d.%s", parent, id, roomId)
+	}
+	if b.LegacyUserId {
+		userId := b.LegacyUserId
+		name := fmt.Sprintf("%s/blurbs/%d~%s", parent, id, userId)
+	}
 	b.Name = name
 	b.CreateTime = now
 	b.UpdateTime = now
