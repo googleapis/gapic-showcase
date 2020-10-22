ebc763453145cb2139b9adbf6f135b465004bc07
Victor Chudnovsky <vchudnov@google.com>
2020-10-22 13:55:12 -0700

    chore: Factor out multiple endpoints into a common runner
    This creates an Endpoint interface wrapping both the gRPC and the
    REST endpoints, as well as the cmux multiplexer that redirects from
    the external port to either of those.
    
    This also sets up the infrastructure for graceful shutdown upon
    receipt of a signal, though we are waiting for a PR in a dependency
    for that to work fully.

--- cmd/gapic-showcase/run.go
+++ cmd/gapic-showcase/run.go
@@ -15,155 +15,43 @@
 package main
 
 import (
-	"crypto/tls"
-	"crypto/x509"
-	"io/ioutil"
-	"log"
-	"net"
-	"net/http"
-	"strings"
+	"os"
+	"os/signal"
+	"syscall"
 
-	"golang.org/x/sync/errgroup"
-
-	"github.com/googleapis/gapic-showcase/server"
-	pb "github.com/googleapis/gapic-showcase/server/genproto"
-	"github.com/googleapis/gapic-showcase/server/services"
-	fallback "github.com/googleapis/grpc-fallback-go/server"
-	"github.com/soheilhy/cmux"
 	"github.com/spf13/cobra"
-	lropb "google.golang.org/genproto/googleapis/longrunning"
-
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials"
-	"google.golang.org/grpc/reflection"
 )
 
-func grpcServe(lis net.Listener, config configuration) error {
-	// Setup Server.
-	logger := &loggerObserver{}
-	observerRegistry := server.ShowcaseObserverRegistry()
-	observerRegistry.RegisterUnaryObserver(logger)
-	observerRegistry.RegisterStreamRequestObserver(logger)
-	observerRegistry.RegisterStreamResponseObserver(logger)
-
-	opts := []grpc.ServerOption{
-		grpc.StreamInterceptor(observerRegistry.StreamInterceptor),
-		grpc.UnaryInterceptor(observerRegistry.UnaryInterceptor),
+func message(err error) string {
+	if err == nil {
+		return "ok"
 	}
-
-	// load mutual TLS cert/key and root CA cert
-	if config.tlsCaCert != "" && config.tlsCert != "" && config.tlsKey != "" {
-		keyPair, err := tls.LoadX509KeyPair(config.tlsCert, config.tlsKey)
-		if err != nil {
-			log.Fatalf("Failed to load server TLS cert/key with error:%v", err)
-		}
-
-		cert, err := ioutil.ReadFile(config.tlsCaCert)
-		if err != nil {
-			log.Fatalf("Failed to load root CA cert file with error:%v", err)
-		}
-
-		pool := x509.NewCertPool()
-		pool.AppendCertsFromPEM(cert)
-
-		ta := credentials.NewTLS(&tls.Config{
-			Certificates: []tls.Certificate{keyPair},
-			ClientCAs:    pool,
-			ClientAuth:   tls.RequireAndVerifyClientCert,
-		})
-
-		opts = append(opts, grpc.Creds(ta))
-	}
-	s := grpc.NewServer(opts...)
-	defer s.GracefulStop()
-
-	// Creates services used by both the gRPC and REST servers
-	echoServer := services.NewEchoServer()
-
-	// Note: we may be able to share a port
-	// for handling gRPC and a regular HTTP
-	// port!
-	// https://godoc.org/google.golang.org/grpc#Server.ServeHTTP
-	// This has many limitations and is
-	// experimental, however. A better option:
-	// issue a 307 in the specified port if
-	// getting gRPC (as per the detection
-	// algorithm in the URL above)
-	// https://tools.ietf.org/html/rfc2616#page-65
-	// https://en.wikipedia.org/wiki/HTTP_302
-
-	// USE THIS: https://medium.com/@drgarcia1986/listen-grpc-and-http-requests-on-the-same-port-263c40cb45ff
-	// which employs cmux: https://github.com/soheilhy/cmux
-
-	// Register Services to the server.
-	pb.RegisterEchoServer(s, echoServer)
-	pb.RegisterSequenceServiceServer(s, services.NewSequenceServer())
-	identityServer := services.NewIdentityServer()
-	pb.RegisterIdentityServer(s, identityServer)
-	messagingServer := services.NewMessagingServer(identityServer)
-	pb.RegisterMessagingServer(s, messagingServer)
-	operationsServer := services.NewOperationsServer(messagingServer)
-	pb.RegisterTestingServer(s, services.NewTestingServer(observerRegistry))
-	lropb.RegisterOperationsServer(s, operationsServer)
-
-	fb := fallback.NewServer(config.fallbackPort, "localhost"+config.port)
-	fb.StartBackground()
-	defer fb.Shutdown()
-
-	// Register reflection service on gRPC server.
-	reflection.Register(s)
-	stdLog.Printf("  listening for gRPC connections")
-	return s.Serve(lis)
-}
-
-func httpServe(lis net.Listener, config configuration) error {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/hello", func(w http.ResponseWriter, _ *http.Request) {
-		w.Write([]byte("GAPIC Showcase: HTTP/REST endpoint\n"))
-	})
-
-	s := &http.Server{Handler: mux}
-	stdLog.Printf("  listening for REST connections")
-	return s.Serve(lis)
-}
-
-type configuration struct {
-	port         string
-	fallbackPort string
-	tlsCaCert    string
-	tlsCert      string
-	tlsKey       string
+	return err.Error()
 }
 
 func init() {
-	config := configuration{}
+	config := RuntimeConfig{}
 	runCmd := &cobra.Command{
 		Use:   "run",
 		Short: "Runs the showcase server",
 		Run: func(cmd *cobra.Command, args []string) {
-			// Ensure port is of the right form.
-			if !strings.HasPrefix(config.port, ":") {
-				config.port = ":" + config.port
-			}
+			cmuxServer := CreateAllEndpoints(config)
 
-			// Start listening.
-			lis, err := net.Listen("tcp", config.port)
-			if err != nil {
-				log.Fatalf("Showcase failed to listen on port '%s': %v", config.port, err)
-			}
-			stdLog.Printf("Showcase listening on port: %s", config.port)
+			done := make(chan os.Signal, 2)
+			signal.Notify(done, os.Interrupt, os.Kill, syscall.SIGTERM, syscall.SIGHUP, syscall.SIGSTOP)
+			go func() {
+				sig := <-done
+				stdLog.Printf("Got signal %q", sig)
+				stdLog.Printf("Shutting down server: %s", message(cmuxServer.Shutdown()))
 
-			m := cmux.New(lis)
-			grpcListener := m.MatchWithWriters(cmux.HTTP2MatchHeaderFieldSendSettings("content-type", "application/grpc"))
-			httpListener := m.Match(cmux.HTTP1Fast())
+				// TODO: Delete the following line once this PR is
+				// merged: https://github.com/soheilhy/cmux/pull/69. The issue is
+				// that the user cannot Ctrl-C the main binary, probably due to
+				// https://github.com/soheilhy/cmux/pull/69#issuecomment-712928041.
+				os.Exit(1)
+			}()
 
-			g := new(errgroup.Group)
-			g.Go(func() error { return grpcServe(grpcListener, config) })
-			g.Go(func() error { return httpServe(httpListener, config) })
-			g.Go(func() error { return m.Serve() })
-
-			stdLog.Printf("after running server: %x\n", g.Wait())
-
+			stdLog.Printf("Server finished: %s", message(cmuxServer.Serve()))
 		},
 	}
 	rootCmd.AddCommand(runCmd)
