fb573c084046e33abad979e57c0196469c8549e2
noahdietz <ndietz@google.com>
2021-02-12 11:37:28 -0800

    chore: generate metadata & use service yaml

--- util/compile_protos.go
+++ util/compile_protos.go
@@ -64,6 +64,8 @@ func CompileProtos(version string) {
 		"--go_gapic_out=" + outDir,
 		"--go_gapic_opt=go-gapic-package=github.com/googleapis/gapic-showcase/client;client",
 		"--go_gapic_opt=grpc-service-config=schema/google/showcase/v1beta1/showcase_grpc_service_config.json",
+		"--go_gapic_opt=api-service-config=schema/google/showcase/v1beta1/showcase_v1beta1.yaml",
+		"--go_gapic_opt=metadata",
 		"--go_rest_server_out=" + filepath.Join("server", "genrest"),
 		"--go_out=plugins=grpc:" + outDir,
 	}
