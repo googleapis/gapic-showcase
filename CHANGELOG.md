ad21d90251d577aeeb6067a778f2e6b61cbd2ed7
noahdietz <ndietz@google.com>
2021-02-19 17:39:00 -0800

    remove circleci

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,8 +1,5 @@
 # Release History
 
-### v0.0.1-test / 2021-02-19
-- testing github actions
-
 ### v0.12.0 / 2020-08-12
 - Add client-side retry/deadline testing surface
 - Regen client & CLI with small updates
--- cmd/gapic-showcase/version.go
+++ cmd/gapic-showcase/version.go
@@ -15,7 +15,7 @@
 package main
 
 func init() {
-	// Make roots version option only emit the version. This is used in circleci.
+	// Make roots version option only emit the version. This is used in Actions.
 	// The template looks weird on purpose. Leaving as a single line causes the
 	// output to append an extra character.
 	rootCmd.Version = "0.12.0"
