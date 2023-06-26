98e9b0a64281c1c9971c87aa482f24989f2b4db2
Lawrence Qiu <lawrenceqiu@google.com>
2023-06-26 10:45:55 -0400

    fix: Use specifc regex for matching URL path

--- util/genrest/resttools/constants.go
+++ util/genrest/resttools/constants.go
@@ -26,12 +26,12 @@ const (
 	// RegexURLPathSingleSegmentValue contains the regex expression for matching a single URL
 	// path segment (i.e. `/` is not allowed). Since gorilla/mux hands uses the decoded paths to
 	// match, we can just accept any characters.
-	RegexURLPathSingleSegmentValue = "[a-zA-Z0-9_\\-]+"
+	RegexURLPathSingleSegmentValue = "[^:\\/]+"
 
 	// RegexURLPathSingleSegmentValue contains the regex expression for matching multiple URL
 	// path segments (i.e. `/` is allowed). Since gorilla/mux hands uses the decoded paths to
 	// match, we can just accept any characters.
-	RegexURLPathMultipleSegmentValue = "[a-zA-Z0-9_\\-\\/]+"
+	RegexURLPathMultipleSegmentValue = "[^:]+"
 )
 
 var (
