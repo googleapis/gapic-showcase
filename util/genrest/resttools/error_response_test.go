8d611088ac9b2dc01cc3d2a6a054bcdafba7026e
Victor Chudnovsky <vchudnov@google.com>
2025-04-03 16:43:27 -0700

    bifurcate test to cover HTTP/gRPC code conversion both ways

--- util/genrest/resttools/error_response_test.go
+++ util/genrest/resttools/error_response_test.go
@@ -26,31 +26,31 @@ import (
 	"google.golang.org/grpc/codes"
 )
 
-func TestErrorResponse(t *testing.T) {
+func TestErrorResponseFromHTTP(t *testing.T) {
 
 	for _, tst := range []struct {
 		details       []interface{}
 		message, name string
-		status        int
+		httpStatus    int
 		wantResponse  string
 	}{
 		{
 			name:         "internal_server",
 			message:      "Had an issue",
-			status:       http.StatusInternalServerError,
+			httpStatus:   http.StatusInternalServerError,
 			details:      []interface{}{&errdetails.ErrorInfo{Reason: "foo"}},
 			wantResponse: `{"error":{"code":500, "message":"Had an issue", "status":"INTERNAL", "details":[{"@type":"type.googleapis.com/google.rpc.ErrorInfo", "reason":"foo"}]}}`,
 		},
 		{
 			name:         "bad_request",
 			message:      "The request was bad",
-			status:       http.StatusBadRequest,
+			httpStatus:   http.StatusBadRequest,
 			wantResponse: `{"error":{"code":400, "message":"The request was bad", "status":"INVALID_ARGUMENT"}}`,
 		},
 		{
-			name:    "multiple_issues",
-			message: "Had multiple issues",
-			status:  http.StatusInternalServerError,
+			name:       "multiple_issues",
+			message:    "Had multiple issues",
+			httpStatus: http.StatusInternalServerError,
 			details: []interface{}{
 				&errdetails.ErrorInfo{Reason: "foo"},
 				&errdetails.BadRequest{
@@ -67,9 +67,81 @@ func TestErrorResponse(t *testing.T) {
 		},
 	} {
 		got := httptest.NewRecorder()
-		ErrorResponse(got, tst.status, NoCodeGRPC, tst.message, tst.details...)
-		if got.Code != tst.status {
-			t.Errorf("%s: Expected code %d, but got %d", tst.name, tst.status, got.Code)
+		ErrorResponse(got, tst.httpStatus, NoCodeGRPC, tst.message, tst.details...)
+		if got.Code != tst.httpStatus {
+			t.Errorf("%s: Expected code %d, but got %d", tst.name, tst.httpStatus, got.Code)
+		}
+		gotResponse, err := io.ReadAll(got.Result().Body)
+		if err != nil {
+			t.Fatalf("%s: Error reading response body: %+v", tst.name, err)
+		}
+		var gotJSON interface{}
+		err = json.Unmarshal([]byte(gotResponse), &gotJSON)
+		if err != nil {
+			t.Fatalf("%s: Error parsing actual response body as JSON: %+v", tst.name, err)
+		}
+
+		var wantJSON interface{}
+		err = json.Unmarshal([]byte(tst.wantResponse), &wantJSON)
+		if err != nil {
+			t.Fatalf("%s: Error parsing expected response body as JSON: %+v", tst.name, err)
+		}
+
+		if diff := cmp.Diff(gotJSON, wantJSON); diff != "" {
+			t.Errorf("%s: error body: got(-),want(+):%s\n\n---------- Raw JSON: got\n%s\n---------- Raw JSON: want\n%s",
+				tst.name, diff, gotResponse, tst.wantResponse)
+		}
+	}
+}
+
+func TestErrorResponseFromGRPC(t *testing.T) {
+
+	for _, tst := range []struct {
+		details        []interface{}
+		message, name  string
+		grpcCode       codes.Code
+		wantHTTPStatus int
+		wantResponse   string
+	}{
+		{
+			name:           "internal_server",
+			message:        "Had an issue",
+			grpcCode:       codes.Internal,
+			details:        []interface{}{&errdetails.ErrorInfo{Reason: "foo"}},
+			wantHTTPStatus: http.StatusInternalServerError,
+			wantResponse:   `{"error":{"code":500, "message":"Had an issue", "status":"INTERNAL", "details":[{"@type":"type.googleapis.com/google.rpc.ErrorInfo", "reason":"foo"}]}}`,
+		},
+		{
+			name:           "bad_request",
+			message:        "The request was bad",
+			grpcCode:       codes.InvalidArgument,
+			wantHTTPStatus: http.StatusBadRequest,
+			wantResponse:   `{"error":{"code":400, "message":"The request was bad", "status":"INVALID_ARGUMENT"}}`,
+		},
+		{
+			name:           "multiple_issues",
+			message:        "Had multiple issues",
+			grpcCode:       codes.Internal,
+			wantHTTPStatus: http.StatusInternalServerError,
+			details: []interface{}{
+				&errdetails.ErrorInfo{Reason: "foo"},
+				&errdetails.BadRequest{
+					FieldViolations: []*errdetails.BadRequest_FieldViolation{
+						{
+							Field:       "an offending field",
+							Description: "a description",
+							Reason:      "a reason",
+						},
+					},
+				},
+			},
+			wantResponse: `{"error":{"code":500, "message":"Had multiple issues", "status":"INTERNAL", "details":[{"@type":"type.googleapis.com/google.rpc.ErrorInfo", "reason":"foo"}, {"@type":"type.googleapis.com/google.rpc.BadRequest", "fieldViolations":[{"field":"an offending field", "description":"a description", "reason":"a reason"}]}]}}`,
+		},
+	} {
+		got := httptest.NewRecorder()
+		ErrorResponse(got, CalculateCodeHTTP, tst.grpcCode, tst.message, tst.details...)
+		if got.Code != tst.wantHTTPStatus {
+			t.Errorf("%s: Expected code %d, but got %d", tst.name, tst.wantHTTPStatus, got.Code)
 		}
 		gotResponse, err := io.ReadAll(got.Result().Body)
 		if err != nil {
